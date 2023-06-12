2c3e7effcc4a8ee27c86b5e04ef58a4c9c598ce9
venky <venkynellore98@gmail.com>
2023-06-12 19:31:44 +0530

    final

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import AuthSocialButton from "@/app/components/AuthSocialButton";
+import { toast } from "react-hot-toast";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/input";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const session = useSession();
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      router.push("/users");
+    }
+  }, [session?.status]);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+      // Axios Register
+      axios
+        .post("/api/register", data)
+        .then(() => signIn("credentials", data))
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false));
+    }
+    if (variant === "LOGIN") {
+      //Next-Auth Sign in
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid Credentials");
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!");
+            router.push("/users");
+          }
+        })
+        .finally(() => setIsLoading(false));
+    }
+  };
+
+  const socialAction = (action: string) => {
+    //Next-Auth Social Signin
+    setIsLoading(true);
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid Credentials");
+        }
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              label="Name"
+              register={register}
+              id="name"
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            label="Email address"
+            type="email"
+            register={register}
+            id="email"
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            label="Password"
+            type="password"
+            register={register}
+            id="password"
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Button disabled={isLoading} fullWidth type="submit">
+            {variant === "LOGIN" ? "Sign In" : "Register"}
+          </Button>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                {" "}
+                Or Continue with{" "}
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onclick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onclick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? `New to Messanger`
+              : `Already have an account?`}
+          </div>
+
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? `Create an account` : `Login`}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+import { useParams, usePathname } from "next/navigation";
+import { HiChat } from "react-icons/hi";
+import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
+
+const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+  const routes = useMemo(
+    () => [
+      {
+        label: "Chat",
+        href: "/conversations", // Add the href property here
+        icon: HiChat,
+        active: pathname === "/conversations" || !!conversationId,
+      },
+      {
+        label: "Users",
+        href: "/users", // Add the href property here
+        icon: HiUsers,
+        active: pathname === "/users",
+      },
+      {
+        label: "LogOut",
+        href: "#",
+        onClick: () => signOut(),
+        icon: HiArrowLeftOnRectangle,
+      },
+    ],
+    [pathname, conversationId]
+  );
+
+  return routes;
+};
+
+export default useRoutes;
